74c463842372a5e41107c1df42b73e26f81edf6a
Steven Buss <sbuss@counsyl.com>
2013-12-07 13:20:11 -0800

    Expand order tests

--- order_test.go
+++ order_test.go
@@ -5,10 +5,22 @@ import (
 )
 
 func TestOrderCreation(t *testing.T) {
-	buy_amount := 1000
-	value_amount := 50
-	order = NewOrder(Buy, buy_amount, value_amount)
-	if order.Type != Buy {
-		t.Errorf("%v != %v", order.Type, Buy)
+	var (
+		amount       uint64
+		value_amount uint64
+	)
+	amount = 1000
+	value_amount = 50
+	for _, buyOrSell := range []OrderType{Buy, Sell} {
+		order := NewOrder(buyOrSell, amount, value_amount)
+		if order.Type != buyOrSell {
+			t.Errorf("%v != %v", order.Type, buyOrSell)
+		}
+		if order.Amount != amount {
+			t.Errorf("%v != %v", order.Amount, amount)
+		}
+		if order.Value != value_amount {
+			t.Errorf("%v != %v", order.Value, value_amount)
+		}
 	}
 }
